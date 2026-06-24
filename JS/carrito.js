6149791ef61504a31491e04e1783cae76ffb8842
romi flores <romi31flores@gmail.com>
2026-06-24 01:35:25 -0300

    modificamos el archivos carrito.js y agregamos comentarios

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,29 +1,36 @@
+// SELECCIONAMOS LOS ELEMENTOS DEL HTML
+// querySelector busca elementos del documento HTML.
+
 const listaCarrito = document.querySelector("#lista-carrito");
 const totalCarrito = document.querySelector("#total");
 
+// ARRAY DEL CARRITO
+// Acá se guardan los productos que el usuario agrega.
+
 let carrito = [];
+
+// FUNCIÓN PARA AGREGAR PRODUCTOS AL CARRITO
 
 function agregarAlCarrito(producto) {
     carrito.push(producto);
     actualizarCarrito();
 }
 
-function actualizarCarrito() {
+// FUNCIÓN PARA ACTUALIZAR EL CARRITO EN PANTALLA
 
+function actualizarCarrito() {
     listaCarrito.innerHTML = "";
 
     let total = 0;
 
-    carrito.forEach((producto) => {
-
+    carrito.forEach(function(producto) {
         const item = document.createElement("li");
 
-        item.textContent =
-            `${producto.nombre} - $${producto.precio}`;
+        item.textContent = producto.nombre + " - $" + producto.precio;
 
         listaCarrito.appendChild(item);
 
-        total += producto.precio;
+        total = total + producto.precio;
     });
 
     totalCarrito.textContent = total;
