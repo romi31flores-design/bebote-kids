a6f3b04df8666dc9eb114bc0521780c1073c92fe
romi flores <romi31flores@gmail.com>
2026-06-24 01:51:08 -0300

    agregamos la seccion de login

---
+++ JS/formulario.js
@@ -0,0 +1,49 @@
+// =====================================
+// SELECCION DEL FORMULARIO
+// =====================================
+
+const formulario = document.querySelector("#formulario");
+
+const mensajeError = document.querySelector("#mensaje-error");
+
+
+// =====================================
+// EVENTO SUBMIT DEL FORMULARIO
+// =====================================
+
+formulario.addEventListener("submit", function(evento) {
+
+    // EVITA RECARGAR LA PAGINA
+
+    evento.preventDefault();
+
+    // OBTIENE LOS VALORES INGRESADOS
+
+    const nombre = document.querySelector("#nombre").value;
+
+    const email = document.querySelector("#email").value;
+
+    const mensaje = document.querySelector("#mensaje").value;
+
+
+    // VALIDACION DE CAMPOS VACIOS
+
+    if (
+        nombre === "" ||
+        email === "" ||
+        mensaje === ""
+    ) {
+
+        mensajeError.textContent =
+        "Complete todos los campos";
+
+    }
+
+    else {
+
+        mensajeError.textContent =
+        "Formulario enviado correctamente";
+
+    }
+
+});
